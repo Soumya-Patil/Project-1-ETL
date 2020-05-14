d0bddd5051a74a26584be4f447073ad919be325c
Soumya Patil <getme1104@gmail.com>
2020-05-14 09:54:39 -0700

    Added the Query SQL file

---
+++ Resources/ETL_Query.sql
@@ -0,0 +1,20 @@
+-- check if the tables been Populated
+SELECT * FROM happiness;
+SELECT * FROM population;
+SELECT * FROM life_expectancy;
+
+-- join tables on the country column
+SELECT
+     a.country AS Country,
+     b.population_2020 AS Population_2020,
+	 b.migrants_net AS Net_Migrants,
+     b.world_share_percent AS World_Share_percent,
+	 c.life_expectancy_both_sexes AS Life_Expectency_both_sexes,
+     a.happiness_score AS Happiness_Score
+    
+FROM
+    happiness AS a
+    INNER JOIN population AS b
+        ON (a.country = b.country)
+    INNER JOIN life_expectancy AS c
+        ON (a.country = c.country);
